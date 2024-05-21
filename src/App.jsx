dc7233a3e8e98b695e4e37fee769dfcc820f60e3
FE_sunmin <fe.sunmin@gmail.com>
2024-05-21 11:39:57 +0900

    Feat: Add Students Info

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import "./App.css";
+
 export default function App() {
   const students = [
     { name: "Alice", age: 17, grade: "A" },
@@ -7,14 +9,23 @@ export default function App() {
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((stud) => stud.age >= 18);
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
-        {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
+        {filteredStudents.map((stud) => (
+          <li
+            className="students-list"
+            key={students.name}
+            // TODO: 학생이름을 클릭하면 나이와 점수가 salert 돼야 해요.
+            onClick={() => alert(`나이: ${stud.age} | 점수: ${stud.grade}`)}
+          >
+            {stud.name}
+          </li>
+        ))}
       </ul>
     </div>
   );
